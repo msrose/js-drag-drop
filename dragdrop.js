79c2758c1b94b65163b672e67c9fb8f1baf615fe
Michael <ekimsc1094@sympatico.ca>
2014-10-05 23:45:15 -0400

    Confine squares to their container. Make script more object-oriented.

--- dragdrop.js
+++ dragdrop.js
@@ -16,110 +16,123 @@ window.dragdrop = (function() {
     return "#" + hexVal;
   }
 
-  return {
-    create: function(containerId, options) {
-      options = options || {};
+  var defaults = {
+    numSquares: 1,
+    squareSize: 100,
+    squareColors: ["gray"],
+    showNumbering: false,
+    randomColors: null,
+    border: { size: 0, format: "solid", color: "black" }
+  };
 
-      var defaults = {
-        numSquares: 1,
-        squareSize: 100,
-        squareColors: ["gray"],
-        showNumbering: false,
-        randomColors: null,
-        border: { size: 0, format: "solid", color: "black" }
-      };
+  function Dragdrop(options) {
+    options = options || {};
 
-      buildOptions(options, defaults);
-      buildOptions(options.border, defaults.border);
+    buildOptions(options, defaults);
+    buildOptions(options.border, defaults.border);
 
-      if(options.randomColors) {
-        var hexMax = parseInt("FFFFFF", 16);
-
-        var colors = [];
-        for(var i = 0; i < options.randomColors; i++) {
-          colors.push(randHex(hexMax));
-        }
-
-        options.squareColors = colors;
+    if(options.randomColors) {
+      var colors = [];
+      for(var i = 0; i < options.randomColors; i++) {
+        colors.push(randHex());
       }
 
-      var container = document.getElementById(containerId);
-      container.style.position = "relative";
-      container.style.overflow = "hidden";
+      options.squareColors = colors;
+    }
 
-      var dragItem = null;
-      var initialLeft;
-      var initialTop;
-      var initialClientX;
-      var initialClientY;
-      var currentHighIndex = 1;
+    for(var property in options) {
+      this[property] = options[property];
+    }
+  }
 
-      var squareStyles = {
-        border: options.border.size + "px " + options.border.format + " " + options.border.color,
-        fontSize: options.squareSize / 2 + "px",
-        height: options.squareSize - options.border.size + "px",
-        width: options.squareSize - options.border.size + "px"
-      };
+  Dragdrop.prototype.draw = function(containerId) {
+    var dragItem = null;
+    var initialLeft;
+    var initialTop;
+    var initialClientX;
+    var initialClientY;
+    var currentHighIndex = 1;
 
-      var squaresPerRow = parseInt(container.offsetWidth / options.squareSize);
-      var col = 0;
-      var row = 0;
+    var squareStyles = {
+      border: this.border.size + "px " + this.border.format + " " + this.border.color,
+      fontSize: this.squareSize / 2 + "px",
+      height: this.squareSize - this.border.size + "px",
+      width: this.squareSize - this.border.size + "px"
+    };
 
-      for(var i = 0; i < options.numSquares; i++) {
-        var newSquare = document.createElement("div");
-        newSquare.className = "dragdrop square";
+    var container = document.getElementById(containerId);
+    container.style.position = "relative";
+    container.style.overflow = "hidden";
 
-        squareStyles.left = col * options.squareSize + "px";
-        squareStyles.top = row * options.squareSize + "px";
+    var squaresPerRow = parseInt(container.offsetWidth / this.squareSize);
+    var col = 0;
+    var row = 0;
 
-        col++;
-        if (col >= squaresPerRow) {
-            col = 0;
-            row++;
-        }
+    for(var i = 0; i < this.numSquares; i++) {
+      var newSquare = document.createElement("div");
+      newSquare.className = "dragdrop square";
 
-        if(options.squareColors.length > 0) {
-          squareStyles.backgroundColor = options.squareColors[i % options.squareColors.length];
-        }
+      squareStyles.left = col * this.squareSize + "px";
+      squareStyles.top = row * this.squareSize + "px";
 
-        for(var property in squareStyles) {
-          newSquare.style[property] = squareStyles[property];
-        }
-
-        newSquare.onmousedown = function(e) {
-          dragItem = this;
-          dragItem.style.zIndex = currentHighIndex;
-          currentHighIndex++;
-          initialTop = dragItem.offsetTop;
-          initialLeft = dragItem.offsetLeft;
-          initialClientY = e.clientY;
-          initialClientX = e.clientX;
-        };
-
-        newSquare.onmouseup = function(e) {
-          dragItem = null;
-        };
-
-        if(options.showNumbering) {
-          var divLabel = document.createElement("span");
-          divLabel.style.lineHeight = options.squareSize + "px";
-          divLabel.innerText = i + 1;
-          newSquare.appendChild(divLabel);
-        }
-
-        container.appendChild(newSquare);
+      col++;
+      if (col >= squaresPerRow) {
+        col = 0;
+        row++;
       }
 
-      container.onmousemove = function(e) {
-        if(dragItem) {
-          dragItem.style.top = initialTop + e.clientY - initialClientY + "px";
-          dragItem.style.left = initialLeft + e.clientX - initialClientX + "px";
-        }
+      if(this.squareColors.length > 0) {
+        squareStyles.backgroundColor = this.squareColors[i % this.squareColors.length];
+      }
+
+      for(var property in squareStyles) {
+        newSquare.style[property] = squareStyles[property];
+      }
+
+      newSquare.onmousedown = function(e) {
+        dragItem = this;
+        dragItem.style.zIndex = currentHighIndex;
+        currentHighIndex++;
+        initialTop = dragItem.offsetTop;
+        initialLeft = dragItem.offsetLeft;
+        initialClientY = e.clientY;
+        initialClientX = e.clientX;
       };
 
-      container.onmouseup = function(e) {
+      newSquare.onmouseup = function(e) {
         dragItem = null;
       };
+
+      if(this.showNumbering) {
+        var divLabel = document.createElement("span");
+        divLabel.style.lineHeight = this.squareSize + "px";
+        divLabel.innerText = i + 1;
+        newSquare.appendChild(divLabel);
+      }
+
+      container.appendChild(newSquare);
     }
+
+    container.onmousemove = function(e) {
+      if(dragItem) {
+        if(this.offsetTop <= e.clientY && e.clientY <= this.offsetTop + this.offsetHeight) {
+          dragItem.style.top = initialTop + e.clientY - initialClientY + "px";
+        }
+        if(this.offsetLeft <= e.clientX && e.clientX <= this.offsetLeft + this.offsetWidth) {
+          dragItem.style.left = initialLeft + e.clientX - initialClientX + "px";
+        }
+      }
+    };
+
+    container.onmouseup = function(e) {
+      dragItem = null;
+    };
+  };
+
+  return {
+    create: function(options) {
+      return new Dragdrop(options);
+    },
+    defaults: defaults
   };
 })();
